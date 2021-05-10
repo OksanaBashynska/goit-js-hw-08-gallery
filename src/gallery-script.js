bf119e4be43b5664fc995db5016fc7ac159d5dbe
Oksana Bashynska <oksanavvisotska@gmail.com>
2021-05-10 11:28:18 +0300

    gallery-script.js

--- src/gallery-script.js
+++ src/gallery-script.js
@@ -3,12 +3,8 @@ import gallery from './gallery-items.js';
 function createImgCardMarkup(images) {
   return images.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${original}"
-  >
-    <img
-      class="gallery__image"
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
@@ -17,20 +13,51 @@ function createImgCardMarkup(images) {
 </li>`}).join('');
 };
 
-const galleryContainer = document.querySelector('.gallery.js-gallery');
+const galleryContainer = document.querySelector('.js-gallery');
 const galleryMarkup = createImgCardMarkup(gallery);
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
+
+const lightboxContainer = document.querySelector('.js-lightbox');
+const modal = document.querySelector('.lightbox__content');
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 
+
+
 function onGalleryContainerClick(evt) {
+  evt.preventDefault();
   if (!evt.target.classList.contains('gallery__image')) {
     return;
   }
-  const galleryEl = evt.target;
+  const bigImgEl = evt.target.alt;
+  let lightboxImage;
+   for (let i = 0; i < gallery.length; i++) {
+    if (gallery[i].description === bigImgEl) {
+      lightboxImage = gallery[i].original;
+    }
+  }
   
-  console.log(galleryEl)
-  const parentGalleryEl = galleryEl.closest('.lightbox.js-lightbox');
-
-  parentGalleryEl.classList.add('.is-open');
+  lightboxContainer.classList.add('is-open'); 
+  modal.innerHTML = `<img class="lightbox__image"
+    src="${lightboxImage}"
+    alt="${bigImgEl}"
+  />`;
 }
+
+
+const btnEl = document.querySelector('button[data-action ="close-lightbox"]');
+btnEl.addEventListener('click', () => {
+  lightboxContainer.classList.remove('is-open');
+});
+
+function isOpen() {
+  const divCloseModal = document.querySelector('.lightbox__image');
+  lightboxContainer.classList.remove('is-open');
+  divCloseModal.alt = '';
+  divCloseModal.src = '';
+}
+
+btnEl.addEventListener('click', isOpen);
+
+const overEl = document.querySelector('.lightbox__overlay');
+overEl.addEventListener('click', isOpen);
